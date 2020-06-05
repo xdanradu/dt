87bcd7a960da7f98a2264501075cf53f09822f20
Dan Radu <dan.radu@msg.group>
2020-06-05 09:14:21 +0300

    vue-spa added

---
+++ lab-2/vue-spa/src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+
+
+Vue.use(VueMaterial);
+
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ lab-2/vue-spa/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="VueJs architecture"/>
+
+        <md-card>
+          <md-card-header>
+            <md-card-header-text>
+              <div class="md-title">Media card</div>
+              <div class="md-subhead">Normal size</div>
+            </md-card-header-text>
+
+            <md-card-media>
+              <img src="../assets/it.jpg" alt="People">
+            </md-card-media>
+          </md-card-header>
+
+          <md-card-actions>
+                <md-button class="md-icon-button md-raised md-primary">
+                  <md-icon>thumb_up</md-icon>
+                </md-button>
+            <md-button class="md-raised md-primary">Update</md-button>
+            <md-button class="md-raised md-accent">Delete</md-button>
+          </md-card-actions>
+        </md-card>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
